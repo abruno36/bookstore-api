9f911335129c0431d8b58df4c2b1c3db4da75b89
= <=>
2021-09-01 20:08:15 -0300

    findById categoria

---
+++ src/main/java/com/bruno/bookstore/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.bruno.bookstore.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruno.bookstore.domain.Categoria;
+import com.bruno.bookstore.exceptions.ObjectNotFoundException;
+import com.bruno.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+}
