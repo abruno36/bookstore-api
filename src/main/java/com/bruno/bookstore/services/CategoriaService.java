7452b3c8cb81b2c00d16bb1e1c231ba95ab4d631
= <=>
2021-09-02 11:53:23 -0300

    tratamento de execeção quando tiver livros na Categoria

--- src/main/java/com/bruno/bookstore/services/CategoriaService.java
+++ src/main/java/com/bruno/bookstore/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.bruno.bookstore.domain.Categoria;
@@ -40,7 +41,15 @@ public class CategoriaService {
 	}
 	
 	public void delete(Integer id) {
-		repository.deleteById(id);
+		findById(id);
+		
+		try {
+			repository.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new com.bruno.bookstore.services.exceptions.DataIntegrityViolationException ("Categoria não pode ser deletada! Possui livros associados");
+		}
+		
 		
 	}
 
