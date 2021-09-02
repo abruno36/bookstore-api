c89ff5917d5acae5f2d5db7b293a153af72b4b8d
= <=>
2021-09-02 10:34:22 -0300

    create Categoria

--- src/main/java/com/bruno/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/bruno/bookstore/resources/CategoriaResource.java
@@ -8,12 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bruno.bookstore.domain.Categoria;
 import com.bruno.bookstore.dtos.CategoriaDTO;
 import com.bruno.bookstore.services.CategoriaService;
+import java.net.URI;
 
 @CrossOrigin("*")
 @RestController
@@ -34,6 +38,13 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
