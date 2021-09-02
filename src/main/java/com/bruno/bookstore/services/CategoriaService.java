72593816cb8f1d59ecb90b6d1fa0420e38d648d1
= <=>
2021-09-02 11:09:44 -0300

    update e delete Categoria

--- src/main/java/com/bruno/bookstore/services/CategoriaService.java
+++ src/main/java/com/bruno/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bruno.bookstore.domain.Categoria;
+import com.bruno.bookstore.dtos.CategoriaDTO;
 import com.bruno.bookstore.repositories.CategoriaRepository;
 import com.bruno.bookstore.services.exceptions.ObjectNotFoundException;
 
@@ -29,6 +30,18 @@ public class CategoriaService {
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+	}
+	
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
+		
 	}
 
 }
